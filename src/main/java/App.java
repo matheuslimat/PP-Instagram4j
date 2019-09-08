97b7e4f27281c40e02acc90cf4547fc130391627
matheuslimat <matheuslimat@gmail.com>
2019-09-08 14:38:53 -0300

    Metodo removeAllUnfollowers e Inclusão de hash code e equals na classe InstagramUserSummary

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,44 +1,92 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.http.client.ClientProtocolException;
 import org.brunocvcunha.instagram4j.Instagram4j;
+import org.brunocvcunha.instagram4j.requests.InstagramFollowRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramGetUserFollowersRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramGetUserFollowingRequest;
 import org.brunocvcunha.instagram4j.requests.InstagramSearchUsernameRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramUnfollowRequest;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramGetUserFollowersResult;
 import org.brunocvcunha.instagram4j.requests.payload.InstagramSearchUsernameResult;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramUserSummary;
+import org.brunocvcunha.instagram4j.requests.payload.StatusResult;
 import org.brunocvcunha.instagram4j.util.PropertiesReader;
 
 public class App {
 	
 	public static Instagram4j instagram = null;
+	public static final Integer TIME_SLEEP_UNFOLLOW = 12000;
 
-	public static void main(String[] args) throws IOException {
+	// ======================== MAIN ==========================
+	public static void main(String[] args) throws IOException, InterruptedException {
 		InputStream inputStream = PropertiesReader.class.getClassLoader().getResourceAsStream("data.properties");
 		
 		Properties prop = new Properties();
 		prop.load(inputStream);
 		
 		login(prop);
+		removeAllUnfollowers(getUnfollowers(getUserByHandle(prop)), getUserByHandle(prop));
 		
 	}
+	// =========================================================
 	
-	public static void login(Properties prop) throws ClientProtocolException, IOException {
+	public static void login(Properties prop) throws ClientProtocolException, IOException, InterruptedException {
 		System.out.println("Realizando login no instagram...");
 		String username = prop.getProperty("user");
 		String password = prop.getProperty("password");
 		instagram = Instagram4j.builder().username(username).password(password).build();
 		instagram.setup();
 		instagram.login();
+
 	}
 	
-	public InstagramSearchUsernameResult getUserByHandle(Properties prop) throws ClientProtocolException, IOException {
+	public static InstagramSearchUsernameResult getUserByHandle(Properties prop) throws ClientProtocolException, IOException {
 		InstagramSearchUsernameResult userResult = instagram.sendRequest(new InstagramSearchUsernameRequest(prop.getProperty("searchUsername")));
 		return userResult;
 	}
 	
-//	public Set<String> getFollowers(InstagramSearchUsernameResult userResult) throws ClientProtocolException, IOException{
-//		InstagramGetUserFollowersResult followers = instagram.sendRequest(new InstagramGetUserFollowersRequest(userResult.getUser().getPk()));
-//		
-//	}
+	// Pega os não seguidores comparando dois resutados
+	public static Set<InstagramUserSummary> getUnfollowers(InstagramSearchUsernameResult userResult) throws ClientProtocolException, IOException{
+		System.out.println("Listando os não seguidores....");
+		InstagramGetUserFollowersResult followers = instagram.sendRequest(new InstagramGetUserFollowersRequest(userResult.getUser().getPk()));
+		InstagramGetUserFollowersResult following = instagram.sendRequest(new InstagramGetUserFollowingRequest(userResult.getUser().getPk()));
+		
+		//lista de seguidores
+		List<InstagramUserSummary> usersFollowers = followers.getUsers();
+		//lista de seguindo
+		List<InstagramUserSummary> usersFollowing = following.getUsers();
+		
+		//Set de não seguidores
+		Set<InstagramUserSummary> unfollowers = new HashSet<InstagramUserSummary>();
+		
+		
+		//percorre a lista de seguindo e coloca no set
+		for (InstagramUserSummary followingUser : usersFollowing) {
+			unfollowers.add(followingUser);
+		}
+		
+		//remove dos seguindo os contido nos seguidores
+		unfollowers.removeAll(usersFollowers);
+		
+		return unfollowers;
+		
+	}
+	
+	public static void removeAllUnfollowers(Set<InstagramUserSummary> unfollowers, InstagramSearchUsernameResult userResult) throws ClientProtocolException, IOException, InterruptedException {
+		for (InstagramUserSummary unfollow : unfollowers) {
+			instagram.sendRequest(new InstagramUnfollowRequest(unfollow.getPk()));
+			Thread.sleep(TIME_SLEEP_UNFOLLOW);
+			System.out.println(unfollow.getUsername() + " Removido!");
+		}
+		System.out.println("Feito! Todos os não seguidores foram removidos do seu instagram.");
+	}
+	
 	
 }
--- src/main/java/org/brunocvcunha/instagram4j/requests/payload/InstagramUserSummary.java
+++ src/main/java/org/brunocvcunha/instagram4j/requests/payload/InstagramUserSummary.java
@@ -40,5 +40,63 @@ public class InstagramUserSummary {
     public String profile_pic_url;
     public boolean has_anonymous_profile_picture;
     public String full_name;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((full_name == null) ? 0 : full_name.hashCode());
+		result = prime * result + (has_anonymous_profile_picture ? 1231 : 1237);
+		result = prime * result + (is_favorite ? 1231 : 1237);
+		result = prime * result + (is_private ? 1231 : 1237);
+		result = prime * result + (is_verified ? 1231 : 1237);
+		result = prime * result + (int) (pk ^ (pk >>> 32));
+		result = prime * result + ((profile_pic_id == null) ? 0 : profile_pic_id.hashCode());
+		result = prime * result + ((profile_pic_url == null) ? 0 : profile_pic_url.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InstagramUserSummary other = (InstagramUserSummary) obj;
+		if (full_name == null) {
+			if (other.full_name != null)
+				return false;
+		} else if (!full_name.equals(other.full_name))
+			return false;
+		if (has_anonymous_profile_picture != other.has_anonymous_profile_picture)
+			return false;
+		if (is_favorite != other.is_favorite)
+			return false;
+		if (is_private != other.is_private)
+			return false;
+		if (is_verified != other.is_verified)
+			return false;
+		if (pk != other.pk)
+			return false;
+		if (profile_pic_id == null) {
+			if (other.profile_pic_id != null)
+				return false;
+		} else if (!profile_pic_id.equals(other.profile_pic_id))
+			return false;
+		if (profile_pic_url == null) {
+			if (other.profile_pic_url != null)
+				return false;
+		} else if (!profile_pic_url.equals(other.profile_pic_url))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+    
+    
     
 }
